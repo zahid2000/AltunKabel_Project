f053ec760ef14e0e7bef21a518762ce56e50522c
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-07-28 17:48:03 +0400

    28 iyul 2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:'home',component:MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
