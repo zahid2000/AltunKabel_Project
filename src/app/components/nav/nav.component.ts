e4f665b2d71daa176a987afad78505c6fec8b105
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-08-08 17:05:48 +0400

    8 avqust 2022

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router ,NavigationEnd, ActivatedRoute } from '@angular/router';
+import {filter} from 'rxjs/operators';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  selectedNavItem = 'home';
+  constructor(private router:Router,private activatedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
+
+
   }
 
 }
