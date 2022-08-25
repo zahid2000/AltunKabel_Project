18021383a51f6d4a4af1a6874954f1ccf2b2f705
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-08-25 22:35:14 +0400

    asa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,58 @@
 export const environment = {
-  production: true
+  production: true,
+  imageList : [
+    {
+      imageSrc: 'assets/images/1.jpg',
+  
+      imageAlt: 'assets/images/1.jpg',
+    },
+    {
+      imageSrc: 'assets/images/2altun.PNG',
+      imageAlt: 'assets/images/2altun.PNG',
+    },
+    {
+      imageSrc: 'assets/images/banner1.jpg',
+      imageAlt: 'assets/images/banner1.jpg',
+    },
+    {
+      imageSrc: 'assets/images/banner2.jpg',
+      imageAlt: 'assets/images/banner2.jpg',
+    },
+    {
+      imageSrc: 'assets/images/1altun.png',
+      imageAlt: 'assets/images/1altun.png',
+    },
+    {
+      imageSrc: 'assets/images/alçaq gərginlikli kabel.jpg',
+      imageAlt: 'assets/images/alçaq gərginlikli kabel.jpg',
+    },
+    {
+      imageSrc: 'assets/images/banner3.jpg',
+      imageAlt: 'assets/images/banner3.jpg',
+    },
+    {
+      imageSrc: 'assets/images/banner4.jpg',
+      imageAlt: 'assets/images/banner4.jpg',
+    },
+    {
+      imageSrc: 'assets/images/banner5.jpg',
+      imageAlt: 'assets/images/banner5.jpg',
+    },
+    {
+      imageSrc: 'assets/images/banner6.jpg',
+      imageAlt: 'assets/images/banner6.jpg',
+    },
+    {
+      imageSrc: 'assets/images/banner7.jpg',
+      imageAlt: 'assets/images/banner7.jpg',
+    },
+    {
+      imageSrc: 'assets/images/banner8.jpg',
+      imageAlt: 'assets/images/banner8.jpg',
+    },
+    {
+      imageSrc: 'assets/images/fire_servival3.jpg',
+      imageAlt: 'assets/images/fire_servival3.jpg',
+    },
+  ]
 };
