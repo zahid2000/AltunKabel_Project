bbf8c337d223d6aa0e3acd31a0adf877b1b98c7b
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-08-25 17:53:39 +0400

    lightbox

--- src/app/components/galery/galery.component.ts
+++ src/app/components/galery/galery.component.ts
@@ -1,116 +1,160 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Lightbox, LightboxConfig } from 'ngx-lightbox';
+interface Item {
+  imageSrc: string;
+  imageAlt: string;
+}
 
 @Component({
   selector: 'app-galery',
   templateUrl: './galery.component.html',
-  styleUrls: ['./galery.component.css'],
+  styleUrls: ['./galery.component.scss'],
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({transform: 'scale(0.5)'}),
+        animate('150ms', style({transform: 'scale(1)'}))
+      ]),
+      transition('visible => void', [
+        style({transform: 'scale(1)'}),
+        animate('150ms', style({transform: 'scale(0.5)'}))
+      ]),
+    ]),
+    trigger('animation2', [
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('50ms', style({opacity: 0.8}))
+      ])
+    ])
+  ]
 })
 export class GaleryComponent implements OnInit {
   breadcrumbitem = 'Qalereya';
    albums: Array <any>= [];
-
-  constructor(private _lightbox: Lightbox,private _lightboxConfig: LightboxConfig) {
+   @Input() galleryData: Item[] = [];
+   @Input() showCount = false;
+ 
+   previewImage = false;
+   showMask = false;
+   currentLightboxImage: Item = this.galleryData[0];
+   currentIndex = 0;
+   controls = true;
+   totalImageCount = 0;
+   //icon
+   faTimes=faTimes
+  constructor() {
   
-    _lightboxConfig.fadeDuration = 1;
-    _lightboxConfig.fitImageInViewPort=true;
-    _lightboxConfig.showZoom=true;
-    _lightboxConfig.showDownloadButton=true;
+   
   }
   
-  ngOnInit(): void {}
-  visiableLightbox: boolean = false;
-  currentIndex: number = 0;
-  currentObj = [];
-
-  title = 'app';
+  ngOnInit(): void {
+    this.totalImageCount = this.galleryData.length;
+  }
+  
 
   _albums:Array<any>= [
     {
-      src: 'assets/images/1.jpg',
+      imageSrc: 'assets/images/1.jpg',
       caption: '',
       thumb: 'assets/images/1.jpg',
     },
     {
-      src: 'assets/images/2altun.PNG',
+      imageSrc: 'assets/images/2altun.PNG',
       thumb: 'assets/images/2altun.PNG',
       caption: '',
     },
     {
-      src: 'assets/images/banner1.jpg',
+      imageSrc: 'assets/images/banner1.jpg',
       thumb: 'assets/images/banner1.jpg',
       caption: '',
     },
     {
-      src: 'assets/images/banner2.jpg',
+      imageSrc: 'assets/images/banner2.jpg',
       thumb: 'assets/images/banner2.jpg',
       caption: '',
     },
     {
-      src: 'assets/images/1altun.png',
+      imageSrc: 'assets/images/1altun.png',
       thumb: 'assets/images/1altun.png',
       caption: '',
     },
     {
-      src: 'assets/images/alçaq gərginlikli kabel.jpg',
+      imageSrc: 'assets/images/alçaq gərginlikli kabel.jpg',
       thumb: 'assets/images/alçaq gərginlikli kabel.jpg',
       caption: '',
     },
     {
-      src: 'assets/images/banner3.jpg',
+      imageSrc: 'assets/images/banner3.jpg',
       thumb: 'assets/images/banner3.jpg',
       caption: '',
     },
     {
-      src: 'assets/images/banner4.jpg',
+      imageSrc: 'assets/images/banner4.jpg',
       thumb: 'assets/images/banner4.jpg',
       caption: '',
     },
     {
-      src: 'assets/images/banner5.jpg',
+      imageSrc: 'assets/images/banner5.jpg',
       thumb: 'assets/images/banner5.jpg',
       caption: '',
     },
     {
-      src: 'assets/images/banner6.jpg',
+      imageSrc: 'assets/images/banner6.jpg',
       thumb: 'assets/images/banner6.jpg',
       caption: '',
     },
     {
-      src: 'assets/images/banner7.jpg',
+      imageSrc: 'assets/images/banner7.jpg',
       thumb: 'assets/images/banner7.jpg',
       caption: '',
     },
     {
-      src: 'assets/images/banner8.jpg',
+      imageSrc: 'assets/images/banner8.jpg',
       thumb: 'assets/images/banner8.jpg',
       caption: '',
     },
     {
-      src: 'assets/images/fire_servival3.jpg',
+      imageSrc: 'assets/images/fire_servival3.jpg',
       thumb: 'assets/images/fire_servival3.jpg',
       caption: '',
     },
   ];
 
-  showLightbox(object: any, index: any) {
+  
+  
+  
+  onPreviewImage(index: number): void {
+    this.showMask = true;
+    this.previewImage = true;
     this.currentIndex = index;
-    this.currentObj = object;
-    console.log('=>', this.currentObj, this.currentIndex);
-    this.visiableLightbox = true;
+    this.currentLightboxImage = this._albums[index];
   }
 
-  closeHandler() {
-    this.visiableLightbox = false;
-    this.currentIndex = -1;
-  }
-  open(index: number): void {
-    // open lightbox
-    this._lightbox.open(this._albums, index, { wrapAround: true, showImageNumberLabel: true });
+  onAnimationEnd(event: any) {
+    if(event.toState === 'void') {
+      this.showMask = false;
+    }
   }
 
-  close(): void {
-    // close lightbox programmatically
-    this._lightbox.close();
+  onClosePreview() {
+    this.previewImage = false;
+  }
+
+  next(): void {
+    this.currentIndex = this.currentIndex + 1;
+    if(this.currentIndex > this._albums.length - 1) {
+      this.currentIndex = 0;
+    }
+    this.currentLightboxImage = this._albums[this.currentIndex];
+  }
+
+  prev(): void {
+    this.currentIndex = this.currentIndex - 1;
+    if(this.currentIndex < 0) {
+      this.currentIndex = this._albums.length - 1;
+    }
+    this.currentLightboxImage = this._albums[this.currentIndex];
   }
 }
