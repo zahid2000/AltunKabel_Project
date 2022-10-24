9be39e2dc6f65cc043194a8a06a777875671d02a
zahid2000 <zahidmamedov07@gmail.com>
2022-10-24 22:22:14 +0400

    Katalog updated

--- src/app/models/imageList.ts
+++ src/app/models/imageList.ts
@@ -4,112 +4,128 @@ export class ImageList {
   constructor() {
     this.galery = [
       {
-        imageSrc: 'assets/images/1.jpg',
-        imageAlt: 'assets/images/1.jpg',
+        imageSrc: 'assets/images/Galery/Galery1.png',
+        imageAlt: 'assets/images/Galery/Galery1.png',
       },
       {
-        imageSrc: 'assets/images/2altun.PNG',
-        imageAlt: 'assets/images/2altun.PNG',
+        imageSrc: 'assets/images/Galery/Galery2.jpg',
+        imageAlt: 'assets/images/Galery/Galery2.jpg',
       },
       {
-        imageSrc: 'assets/images/banner1.jpg',
-        imageAlt: 'assets/images/banner1.jpg',
+        imageSrc: 'assets/images/Galery/Galery3.jpg',
+        imageAlt: 'assets/images/Galery/Galery3.jpg',
       },
       {
-        imageSrc: 'assets/images/banner2.jpg',
-        imageAlt: 'assets/images/banner2.jpg',
+        imageSrc: 'assets/images/Galery/Galery4.jpg',
+        imageAlt: 'assets/images/Galery/Galery4.jpg',
       },
       {
-        imageSrc: 'assets/images/1altun.png',
-        imageAlt: 'assets/images/1altun.png',
+        imageSrc: 'assets/images/Galery/Galery5.jpg',
+        imageAlt: 'assets/images/Galery/Galery5.jpg',
       },
       {
-        imageSrc: 'assets/images/alçaq gərginlikli kabel.jpg',
-        imageAlt: 'assets/images/alçaq gərginlikli kabel.jpg',
+        imageSrc: 'assets/images/Galery/Galery6.jpg',
+        imageAlt: 'assets/images/Galery/Galery6.jpg',
       },
       {
-        imageSrc: 'assets/images/banner3.jpg',
-        imageAlt: 'assets/images/banner3.jpg',
+        imageSrc: 'assets/images/Galery/Galery7.jpg',
+        imageAlt: 'assets/images/Galery/Galery7.jpg',
       },
       {
-        imageSrc: 'assets/images/banner4.jpg',
-        imageAlt: 'assets/images/banner4.jpg',
+        imageSrc: 'assets/images/Galery/Galery8.jpg',
+        imageAlt: 'assets/images/Galery/Galery8.jpg',
       },
       {
-        imageSrc: 'assets/images/banner5.jpg',
-        imageAlt: 'assets/images/banner5.jpg',
+        imageSrc: 'assets/images/Galery/Galery9.jpg',
+        imageAlt: 'assets/images/Galery/Galery9.jpg',
       },
       {
-        imageSrc: 'assets/images/banner6.jpg',
-        imageAlt: 'assets/images/banner6.jpg',
+        imageSrc: 'assets/images/Galery/Galery10.jpg',
+        imageAlt: 'assets/images/Galery/Galery10.jpg',
       },
       {
-        imageSrc: 'assets/images/banner7.jpg',
-        imageAlt: 'assets/images/banner7.jpg',
+        imageSrc: 'assets/images/Galery/Galery11.jpg',
+        imageAlt: 'assets/images/Galery/Galery11.jpg',
       },
       {
-        imageSrc: 'assets/images/banner8.jpg',
-        imageAlt: 'assets/images/banner8.jpg',
+        imageSrc: 'assets/images/Galery/Galery12.jpg',
+        imageAlt: 'assets/images/Galery/Galery12.jpg',
       },
       {
-        imageSrc: 'assets/images/fire_servival3.jpg',
-        imageAlt: 'assets/images/fire_servival3.jpg',
+        imageSrc: 'assets/images/Galery/Galery13.jpg',
+        imageAlt: 'assets/images/Galery/Galery13.jpg',
+      },
+      {
+        imageSrc: 'assets/images/Galery/Galery14.jpg',
+        imageAlt: 'assets/images/Galery/Galery14.jpg',
+      },
+      {
+        imageSrc: 'assets/images/Galery/Galery15.jpg',
+        imageAlt: 'assets/images/Galery/Galery15.jpg',
+      },
+      {
+        imageSrc: 'assets/images/Galery/Galery16.jpg',
+        imageAlt: 'assets/images/Galery/Galery16.jpg',
+      },
+      {
+        imageSrc: 'assets/images/Galery/Galery17.jpg',
+        imageAlt: 'assets/images/Galery/Galery17.jpg',
       },
     ];
-    this.products= [
-        {
-          imageSrc: 'assets/images/1.jpg',
+    // this.products= [
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
       
-          imageAlt: 'assets/images/1.jpg',
-        },
-        {
-          imageSrc: 'assets/images/2altun.PNG',
-          imageAlt: 'assets/images/2altun.PNG',
-        },
-        {
-          imageSrc: 'assets/images/banner1.jpg',
-          imageAlt: 'assets/images/banner1.jpg',
-        },
-        {
-          imageSrc: 'assets/images/banner2.jpg',
-          imageAlt: 'assets/images/banner2.jpg',
-        },
-        {
-          imageSrc: 'assets/images/1altun.png',
-          imageAlt: 'assets/images/1altun.png',
-        },
-        {
-          imageSrc: 'assets/images/alçaq gərginlikli kabel.jpg',
-          imageAlt: 'assets/images/alçaq gərginlikli kabel.jpg',
-        },
-        {
-          imageSrc: 'assets/images/banner3.jpg',
-          imageAlt: 'assets/images/banner3.jpg',
-        },
-        {
-          imageSrc: 'assets/images/banner4.jpg',
-          imageAlt: 'assets/images/banner4.jpg',
-        },
-        {
-          imageSrc: 'assets/images/banner5.jpg',
-          imageAlt: 'assets/images/banner5.jpg',
-        },
-        {
-          imageSrc: 'assets/images/banner6.jpg',
-          imageAlt: 'assets/images/banner6.jpg',
-        },
-        {
-          imageSrc: 'assets/images/banner7.jpg',
-          imageAlt: 'assets/images/banner7.jpg',
-        },
-        {
-          imageSrc: 'assets/images/banner8.jpg',
-          imageAlt: 'assets/images/banner8.jpg',
-        },
-        {
-          imageSrc: 'assets/images/fire_servival3.jpg',
-          imageAlt: 'assets/images/fire_servival3.jpg',
-        },
-      ]
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.PNG',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.PNG',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.png',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.png',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg gərginlikli kabel.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg gərginlikli kabel.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //     {
+    //       imageSrc: 'assets/images/Galery/Galery1.jpg.jpg',
+    //       imageAlt: 'assets/images/Galery/Galery1.jpg.jpg',
+    //     },
+    //   ]
   }
 }
